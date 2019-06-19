36cfcd1f5d36e886778515ea9690b2cb89889afb
Jay Chen <jaychen@buffalo.edu>
2019-06-19 16:58:27 -0400

    Add files via upload

---
+++ RemoveNumFromList.py
@@ -0,0 +1,25 @@
+import re
+
+def remove(list):
+    pattern = '[0-9]'
+    pattern1 = '\n'
+    list = [re.sub(pattern, '', i) for i in list]
+    list = [re.sub(pattern1, '', i) for i in list]
+    return list
+
+text_file = open("testfile.txt", "r")
+
+list1 = text_file.readlines()
+
+your_data = remove(list1)
+
+#print(*your_data, sep = "\n")
+
+print(*your_data, sep = "\n", file=open("output.txt","w"))
+
+
+
+
+
+
+
